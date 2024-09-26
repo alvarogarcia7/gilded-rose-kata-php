e476ab61b039b31fe9aba5c97d847b162deb2578
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-09-27 02:45:07 +0400

    Copy the PHP version from Emily Bache's Gilded Rose Refactoring Kata
    Folder: https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/main/php
    Commit: https://github.com/emilybache/GildedRose-Refactoring-Kata/commit/79288dcd51ff9f3264bf7dce23f28ef5bae29abc

---
+++ tests/GildedRoseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kata\Test;
+
+use Kata\GildedRose;
+use Kata\Item;
+use PHPUnit\Framework\TestCase;
+
+class GildedRoseTest extends TestCase
+{
+    public function testFoo(): void
+    {
+        $items = [new Item('foo', 0, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame('foo', $items[0]->name);
+    }
+}
